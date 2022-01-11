f6c24f0afb61677a64587925d4a4cd38a686234b
Larrire <linekerlarrire@gmail.com>
2022-01-11 07:11:35 -0300

    Fixing spelling mistake and creating test for validate unique name for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it { is_expect.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+
+  
 end
