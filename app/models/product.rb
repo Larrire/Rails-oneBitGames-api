e50ba0721a4a79c3d8f5942525145e6b3fdd166f
Larrire <linekerlarrire@gmail.com>
2022-01-13 07:02:32 -0300

    Removing 'presence: true' from 'has_one_attached' declaration at Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,6 @@ class Product < ApplicationRecord
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
 
-  has_one_attached :image, presence: true
+  has_one_attached :image
   validates :image, presence: true
 end
