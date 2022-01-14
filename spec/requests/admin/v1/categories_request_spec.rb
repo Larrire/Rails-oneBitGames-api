cc8011130d8f4e11608b61b6a11f7d160e651763
Larrire <linekerlarrire@gmail.com>
2022-01-14 06:30:55 -0300

    Adding some modifications and fixes at tests

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -4,19 +4,20 @@ RSpec.describe "Admin::V1::Categories", type: :request do
   let(:user) { create(:user) }
 
   context "GET /categories" do
-    let(:url) { "admin/v1/categories" }
+    let(:url) { "/admin/v1/categories" }
     # Using ! makes RSpec run instantly the command. 
     # We need it to make sure the 5 categories already exists at database when the request is made. 
     let!(:categories) { create_list(:category, 5) }
 
     it "returns all categories" do
       get url, headers: auth_header(user)
-      expect(body_json['categories']).to contains_exacly categories.as_json(only: %i(id name))
+      # Using * makes the "expand" of and array, it's king of like a spread
+      expect(body_json['categories']).to contain_exactly *categories.as_json(only: %i(id name))
     end
 
     it "return success status" do
       get url, headers: auth_header(user)
-      expect(request).to have_http_status(:ok)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
