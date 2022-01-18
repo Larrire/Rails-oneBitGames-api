544d0f4a9daf354f2c2d06ab5cafd0e38f2472ac
Larrire <linekerlarrire@gmail.com>
2022-01-18 07:04:21 -0300

    Creating POST request specs for SystemRequirementsController

--- spec/requests/admin/v1/system_requirements/system_requirements_spec.rb
+++ spec/requests/admin/v1/system_requirements/system_requirements_spec.rb
@@ -11,13 +11,39 @@ RSpec.describe "Admin::V1::SystemRequirements", type: :request do
       expect(body_json['system_requirements']).to contain_exactly *system_requirements.as_json(only: %i(id name))
     end
 
-    it "returns status code :ok" do
+    it "returns success status" do
       get url, headers: auth_header(user)
       expect(response).to have_http_status(:ok)
     end
   end
 
   context "POST /system_requirements" do
+    let(:url) { "/admin/v1/system_requirements" }
+    let(:user) { create(:user) }
+
+    context "with valid params" do
+      let(:system_requirement_params) { {system_requirement: attributes_for(:system_requirement)}.to_json }
+
+      it "adds a new SystemRequirement" do
+        expect do
+          post url, headers: auth_header(user), params: system_requirement_params
+        end.to change(SystemRequirement, :count).by(1)
+      end
+
+      it "returns last added SystemRequirement" do
+        post url, headers: auth_header(user), params: system_requirement_params
+        expected_system_requirement = SystemRequirement.last.as_json( only: %i(id name) )
+        expect(body_json['system_requirement']).to eq expected_system_requirement
+      end
+
+      it "returns success status" do
+        post url, headers: auth_header(user), params: system_requirement_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+
+
   end
 
   context "PATCH /system_requirements/:id" do
