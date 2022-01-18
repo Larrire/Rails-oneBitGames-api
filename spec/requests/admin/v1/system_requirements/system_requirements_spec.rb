5e8c5d4088b37cf2f523568ebc3d02169b7abe27
Larrire <linekerlarrire@gmail.com>
2022-01-18 17:20:53 -0300

    Creating POST request specs for invalid params to SystemRequirementsCOntroller

--- spec/requests/admin/v1/system_requirements/system_requirements_spec.rb
+++ spec/requests/admin/v1/system_requirements/system_requirements_spec.rb
@@ -41,9 +41,57 @@ RSpec.describe "Admin::V1::SystemRequirements", type: :request do
         expect(response).to have_http_status(:ok)
       end
     end
+
+    context "with invalid params" do
+      let(:invalid_system_requirement_params) { {
+        name: nil,
+        operational_system: nil,
+        storage: nil,
+        processor: nil,
+        memory: nil,
+        video_board: nil,
+      }.to_json}
+
+      # let(:invalid_system_requirement_params) do
+      #   { system_requirement: attributes_for(:system_requirement, name: nil) }.to_json
+      # end
+
+      it "does not add a new SystemRequirement" do
+        expect do
+          post url, headers: auth_header(user), params: invalid_system_requirement_params
+        end.to_not change(SystemRequirement, :count)
+      end
+
+      it "returns errors messages" do
+        post url, headers: auth_header(user), params: invalid_system_requirement_params
+        expect(body_json['errors']['fields'].keys).to eq ['name', 'operational_system', 'storage', 'processor', 'memory', 'video_board']
+      end
+
+      it "returns unprocessable_entity status" do
+        post url, headers: auth_header(user), params: invalid_system_requirement_params
+        expect(response).to have_http_status(:unprocessable_entity) # 422
+      end
+
+    end
+
   end # POST
 
   context "PATCH /system_requirements/:id" do
+    let(:system_requirement) { create(:system_requirement) }
+    let(:url) { "/admin/v1/system_requirements/#{system_requirement.id}" }
+    let(:user) { create(:user) }
+    let(:new_attributes) {{
+      name: 'New name',
+      operational_system: 'New oparational system',
+      storage: 'New storage',
+      processor: 'New processor',
+      memory: 'New memory',
+      video_board: 'New video board'
+    }}
+
+
+
+    
   end # PATCH
 
   context "DELETE /system_requirements/:id" do
