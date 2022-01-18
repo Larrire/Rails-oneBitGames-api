f76303c26d03a4150e6090a56371e6a15c45ebf8
Larrire <linekerlarrire@gmail.com>
2022-01-18 06:25:41 -0300

    Creating GET request specs for SystemRequirements Controller

---
+++ spec/requests/admin/v1/system_requirements/system_requirements_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::V1::SystemRequirements", type: :request do
+  context "GET /system_requirements" do
+    let(:system_requirements) { create_list(:system_requirement, 5) }
+    let(:url) { "/admin/v1/system_requirements" }
+    let(:user) { create(:user) }
+
+    it "returns all system requirements" do
+      get url, headers: auth_header(user)
+      expect(body_json['system_requirements']).to contain_exactly *system_requirements.as_json(only: %i(id name))
+    end
+
+    it "returns status code :ok" do
+      get url, headers: auth_header(user)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context "POST /system_requirements" do
+  end
+
+  context "PATCH /system_requirements/:id" do
+  end
+
+  context "DELETE /system_requirements/:id" do
+  end
+end
