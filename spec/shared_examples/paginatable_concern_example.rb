c61ff802fe0560174c8eb704ca9ccc3d9fcb5ee3
Larrire <linekerlarrire@gmail.com>
2022-01-17 23:50:44 -0300

    Fixing some spelling mistakes

--- spec/shared_examples/paginatable_concern_example.rb
+++ spec/shared_examples/paginatable_concern_example.rb
@@ -1,5 +1,4 @@
 shared_examples "paginatable concern" do |factory_name|
-  
   context "when records fits page size" do
     let!(:records) { create_list(factory_name, 20) }
 
@@ -47,7 +46,7 @@ shared_examples "paginatable concern" do |factory_name|
         first_record_index = 5
         last_record_index = 9
         expected_records = described_class.all[first_record_index..last_record_index]
-        expect(paginated_records).eq expected_records
+        expect(paginated_records).to eq expected_records
       end
     end
   
@@ -69,7 +68,7 @@ shared_examples "paginatable concern" do |factory_name|
   end # context "when records fits page size"
 
   context "when records does not fit page size" do
-    let(:records) { create_list(factory_name, 7) }
+    let!(:records) { create_list(factory_name, 7) }
 
     context "when :page and :length are empty" do
       it "returns 7 records" do
