55a6e5659455f21b35994a9881fc63f9cfb37edd
Larrire <linekerlarrire@gmail.com>
2022-01-20 06:41:09 -0300

    Creating index method for CouponsController

--- app/controllers/admin/v1/coupons_controller.rb
+++ app/controllers/admin/v1/coupons_controller.rb
@@ -1,4 +1,14 @@
 module Admin::V1
   class CouponsController < ApiController
+    def index
+      @coupons = Coupon.all
+    end
+
+    def coupon_params
+      return {} unless params.present?
+      params.permit(%i(
+        code status discount_value due_date 
+      ))
+    end
   end
 end
