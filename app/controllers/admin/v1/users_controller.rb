d8fb556b8b928a22d9795c08af3d6229d0d1da3a
Larrire <linekerlarrire@gmail.com>
2022-01-25 20:19:45 -0300

    Creating all UserController methods needed for the API

---
+++ app/controllers/admin/v1/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+
+  end
+
+  def load_user
+
+  end
+
+  def save_user!
+end
