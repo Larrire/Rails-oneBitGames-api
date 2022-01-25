19f9d75c7de4a1e7c6931ec52f0a659b8ba0c61b
Larrire <linekerlarrire@gmail.com>
2022-01-25 06:17:38 -0300

    Creating 'create' method for CouponsController

--- app/controllers/admin/v1/coupons_controller.rb
+++ app/controllers/admin/v1/coupons_controller.rb
@@ -1,14 +1,31 @@
 module Admin::V1
   class CouponsController < ApiController
+    
     def index
       @coupons = Coupon.all
     end
 
+    def create
+      @coupon = Coupon.new
+      @coupon.attributes = coupon_params
+      save_coupon!
+    end
+
+    private
+
     def coupon_params
-      return {} unless params.present?
-      params.permit(%i(
+      return {} unless params.has_key?(:coupon)
+      params.require(:coupon).permit( %i(
         code status discount_value due_date 
       ))
     end
+
+    def save_coupon!
+      @coupon.save!
+      render :show
+    rescue
+      render_error(fields: @coupon.errors.messages)
+    end
+
   end
 end
