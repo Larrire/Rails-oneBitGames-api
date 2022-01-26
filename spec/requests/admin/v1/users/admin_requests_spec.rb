e3f24d4a457ab590df0b6b02d676437883096efb
Larrire <linekerlarrire@gmail.com>
2022-01-25 21:19:42 -0300

    Creating POST request specs for UserController

--- spec/requests/admin/v1/users/admin_requests_spec.rb
+++ spec/requests/admin/v1/users/admin_requests_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::V1::Users as :admin", type: :request do
-  let(:requestUser) { create(:user) }
+  let!(:requestUser) { create(:user) }
 
   context "GET /users" do
     let(:url) { "/admin/v1/users" }
@@ -10,7 +10,7 @@ RSpec.describe "Admin::V1::Users as :admin", type: :request do
     it "returns all users" do
       get url, headers: auth_header(requestUser)
       allUsers = User.all
-      expect(body_json['users']).to contain_exactly *allUsers.as_json(only: %i(name email profile))
+      expect(body_json['users']).to contain_exactly *allUsers.as_json(only: %i(id name email profile))
     end
 
     it "returns success status" do
@@ -20,19 +20,26 @@ RSpec.describe "Admin::V1::Users as :admin", type: :request do
   end #GET
 
   context "POST /users" do
+    let(:url) { "/admin/v1/users" }
 
     context "with valid params" do
+      let(:user_params) { {user: attributes_for(:user)}.to_json }
 
       it "adds a new User" do
-
+        expect do
+          post url, headers: auth_header(requestUser), params: user_params
+        end.to change(User, :count).by(1)
       end
 
       it "returns last added User" do
-
+        post url, headers: auth_header(requestUser), params: user_params
+        expected_user = User.last.as_json(only: %i(id name email profile))
+        expect(body_json['user']).to eq expected_user
       end
 
       it "returns success status" do
-
+        post url, headers: auth_header(requestUser), params: user_params
+        expect(response).to have_http_status(:ok)
       end
 
     end #valid-params
