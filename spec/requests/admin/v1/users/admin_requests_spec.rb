22f334966a0db9eada93dd91296ac3a059bb8a8a
Larrire <linekerlarrire@gmail.com>
2022-01-25 21:50:02 -0300

    Implementing GET:id request specs for UsersController

--- spec/requests/admin/v1/users/admin_requests_spec.rb
+++ spec/requests/admin/v1/users/admin_requests_spec.rb
@@ -65,6 +65,43 @@ RSpec.describe "Admin::V1::Users as :admin", type: :request do
     end #invalid-params
   end #POST
 
-  
+  context "GET /users/:id" do
+
+    context "with a valid id" do
+      let(:url) { "/admin/v1/users/#{requestUser.id}" }
+
+      it "returns requested User" do
+        get url, headers: auth_header(requestUser)
+        expected_user = requestUser.as_json(only: %i(id name email profile))
+        expect(body_json['user']).to eq expected_user
+      end
+
+      it "returns success status" do
+        get url, headers: auth_header(requestUser)
+        expect(response).to have_http_status(:ok)
+      end
+    end #valid-id
+
+    context "with an invalid id" do
+      let(:url) { "/admin/v1/users/99999" }
+
+      it "does not return requested User" do
+        get url, headers: auth_header(requestUser)
+        expect(body_json['user']).to_not be_present
+      end
+
+      it "returns error messages" do
+        get url, headers: auth_header(requestUser)
+        expect(body_json['errors']['message']).to eq 'User not found'
+      end
+
+      it "returns unprocessable_entity status" do
+        get url, headers: auth_header(requestUser)
+        expect(response).to have_http_status(:unprocessable_entity) #422
+      end
+
+    end #invalid-id
+    
+  end #GET
 
 end
