146531742d02720fbfdf60ca3a00a65fb49b52d9
Larrire <linekerlarrire@gmail.com>
2022-01-31 05:55:47 -0300

    Adding :key and :ggame_id columns at license migration

--- db/migrate/20220131085002_create_licenses.rb
+++ db/migrate/20220131085002_create_licenses.rb
@@ -1,6 +1,8 @@
 class CreateLicenses < ActiveRecord::Migration[6.0]
   def change
     create_table :licenses do |t|
+      t.string :key
+      t.references :game, null: false, foreign_key: true
 
       t.timestamps
     end
